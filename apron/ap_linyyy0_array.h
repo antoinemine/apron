1d345d89ceaf86138520612dc7d688157e05e162
Bertrand Jeannet <bertrand.jeannet@argosim.com>
2010-09-09 10:36:41 +0000

    Generators fully added

---
+++ apron/ap_linyyy0_array.h
@@ -0,0 +1,105 @@
+/* -*- mode: c -*- */
+
+/* ************************************************************************* */
+/* ap_linyyy0_array.h: array of ap_linyyy0_t */
+/* ************************************************************************* */
+
+/* This file is part of the APRON Library, released under LGPL license.  Please
+   read the COPYING file packaged in the distribution */
+
+/* normally included from ap_expr0.h */
+
+#ifndef _AP_linyyy0_ARRAY_H_
+#define _AP_linyyy0_ARRAY_H_
+
+#include "ap_linyyy0.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ====================================================================== */
+/* Datatypes */
+/* ====================================================================== */
+
+/* Array of linear expressions */
+typedef struct ap_linyyy0_array_struct {
+  ap_scalar_discr_t discr;
+  union {
+    ap_linyyyD_array_ptr    D;
+    ap_linyyyMPQ_array_ptr  MPQ;
+    ap_linyyyMPFR_array_ptr MPFR;
+  } linyyy_array;
+} ap_linyyy0_array_struct;
+typedef ap_linyyy0_array_struct* ap_linyyy0_array_ptr;
+typedef ap_linyyy0_array_struct ap_linyyy0_array_t[1];
+
+/* ====================================================================== */
+/* I. Memory management and printing */
+/* ====================================================================== */
+
+void ap_linyyy0_array_init(ap_linyyy0_array_t e, ap_scalar_discr_t discr, size_t size);
+void ap_linyyy0_array_init_set(ap_linyyy0_array_t res, ap_linyyy0_array_t e);
+MACRO_MAINZ
+void ap_linyyy0_array_init_set_linyyyZZZ_array(ap_linyyy0_array_t res, ap_linyyyZZZ_array_t e);
+ENDMACRO
+void ap_linyyy0_array_minimize(ap_linyyy0_array_t a);
+void ap_linyyy0_array_clear(ap_linyyy0_array_t e);
+void ap_linyyy0_array_fprint(FILE* stream, ap_linyyy0_array_t e, char** name_of_dim);
+
+/* ====================================================================== */
+/* II. Access, possibly with conversions */
+/* ====================================================================== */
+
+static inline ap_scalar_discr_t ap_linyyy0_array_discr(ap_linyyy0_array_t p);
+size_t ap_linyyy0_array_size(ap_linyyy0_array_t p);
+#if !defined(_AP_lingen0_ARRAY_H_)
+ap_linexpr_type_t ap_linyyy0_array_type(ap_linyyy0_array_t array);
+bool ap_linyyy0_array_is_quasilinear(ap_linyyy0_array_t array);
+#endif
+bool ap_linyyy0_array_is_linear(ap_linyyy0_array_t array);
+  /* Are all the constraints involved linear (resp. quasilinear) */
+
+bool ap_linyyy0_array_set(ap_linyyy0_array_t res, ap_linyyy0_array_t e, num_internal_t intern);
+bool ap_linyyy0_array_set_index(ap_linyyy0_array_t p, size_t i, ap_linyyy0_t a, num_internal_t intern);
+bool ap_linyyy0_array_get_index(ap_linyyy0_t a, ap_linyyy0_array_t p, size_t i, num_internal_t intern);
+void ap_linyyy0_array_ref_index(ap_linyyy0_t a, ap_linyyy0_array_t p, size_t i);
+
+MACRO_MAINZ
+bool ap_linyyy0_array_set_linyyyZZZ_array(ap_linyyy0_array_t a, ap_linyyyZZZ_array_t b, num_internal_t intern);
+bool ap_linyyyZZZ_array_set_linyyy0_array(ap_linyyyZZZ_array_t a, ap_linyyy0_array_t b, num_internal_t intern);
+
+bool ap_linyyy0_array_set_index_linyyyZZZ(ap_linyyy0_array_t p, size_t i, ap_linyyyZZZ_t a, num_internal_t intern);
+bool ap_linyyy0_array_get_index_linyyyZZZ(ap_linyyyZZZ_t a, ap_linyyy0_array_t p, size_t i, num_internal_t intern);
+ENDMACRO
+
+/* ====================================================================== */
+/* III. Change of dimensions and permutations */
+/* ====================================================================== */
+
+/* This function add dimensions to the expressions, following the
+   semantics of dimchange (see the type definition of dimchange).  */
+void ap_linyyy0_array_add_dimensions(ap_linyyy0_array_t a,
+				  ap_linyyy0_array_t b,
+				  ap_dimchange_t* dimchange);
+
+/* This function apply the given permutation to the dimensions. If dense
+   representation, the size of the permutation should be expr->size. If sparse
+   representation, the dimensions present in the expression should just be less
+   than the size of the permutation. */
+void ap_linyyy0_array_permute_dimensions(ap_linyyy0_array_t a,
+				      ap_linyyy0_array_t b,
+				      ap_dimperm_t* perm);
+
+static inline ap_scalar_discr_t ap_linyyy0_array_discr(ap_linyyy0_array_t p)
+{ return p->discr; }
+MACRO_MAINZ
+static inline void ap_linyyy0_array_cons_ZZZ(ap_linyyy0_array_t res, ap_linyyyZZZ_array_t e)
+{ res->discr = AP_SCALAR_ZZZ; res->linyyy_array.ZZZ = e; };
+ENDMACRO
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- apron/ap_linyyy1.c
+++ apron/ap_linyyy1.c
@@ -1,89 +1,89 @@
 /* ************************************************************************* */
-/* ap_yyy1.c: linear expressions or constraints */
+/* ap_linyyy1.c: linear expressions or constraints */
 /* ************************************************************************* */
 
 /* This file is part of the APRON Library, released under LGPL license.  Please
    read the COPYING file packaged in the distribution */
 
-#include "ap_yyy1.h"
+#include "ap_linyyy1.h"
 
-#define _AP_yyy1_MARK_
+#define _AP_linyyy1_MARK_
 
 /* ====================================================================== */
 /* I. Memory management and printing */
 /* ====================================================================== */
 
-void ap_yyy1_init(ap_yyy1_t res, ap_scalar_discr_t discr, size_t size, ap_environment_t* env)
+void ap_linyyy1_init(ap_linyyy1_t res, ap_scalar_discr_t discr, size_t size, ap_environment_t* env)
 {
-  ap_yyy0_init(res->yyy0,discr,size);
+  ap_linyyy0_init(res->linyyy0,discr,size);
   res->env = ap_environment_copy(env);
 }
-void ap_yyy1_init_set(ap_yyy1_t res, ap_yyy1_t e)
+void ap_linyyy1_init_set(ap_linyyy1_t res, ap_linyyy1_t e)
 {
   if (res!=e){
-    ap_yyy0_init_set(res->yyy0,e->yyy0);
+    ap_linyyy0_init_set(res->linyyy0,e->linyyy0);
     res->env = ap_environment_copy(e->env);
   }
 }
-void ap_yyy1_init_set_linexpr0(ap_yyy1_t res, ap_yyy0_t e, ap_environment_t* env)
+void ap_linyyy1_init_set_linexpr0(ap_linyyy1_t res, ap_linyyy0_t e, ap_environment_t* env)
 {
-  ap_yyy0_init_set(res->yyy0,e);
+  ap_linyyy0_init_set(res->linyyy0,e);
   res->env = ap_environment_copy(env);
  }
 
 MACRO_MAINZ
-void ap_yyy1_init_set_yyyZZZ(ap_yyy1_t res, ap_yyyZZZ_t e, ap_environment_t* env)
+void ap_linyyy1_init_set_linyyyZZZ(ap_linyyy1_t res, ap_linyyyZZZ_t e, ap_environment_t* env)
 {
-  ap_yyy0_init_set_yyyZZZ(res->yyy0,e);
+  ap_linyyy0_init_set_linyyyZZZ(res->linyyy0,e);
   res->env = ap_environment_copy(env);
 }
 ENDMACRO
 
-void ap_yyy1_clear(ap_yyy1_t e)
+void ap_linyyy1_clear(ap_linyyy1_t e)
 {
-  ap_yyy0_clear(e->yyy0);
+  ap_linyyy0_clear(e->linyyy0);
   ap_environment_free(e->env);
 }
-void ap_yyy1_fprint(FILE* stream, ap_yyy1_t e)
+void ap_linyyy1_fprint(FILE* stream, ap_linyyy1_t e)
 {
   ap_environment_name_of_dim_t* name_of_dim;
 
   name_of_dim = ap_environment_name_of_dim_alloc(e->env);
-  ap_yyy0_fprint(stream, e->yyy0, name_of_dim->p);
+  ap_linyyy0_fprint(stream, e->linyyy0, name_of_dim->p);
   ap_environment_name_of_dim_free(name_of_dim);
 }
-void ap_yyy1_minimize(ap_yyy1_t e)
+void ap_linyyy1_minimize(ap_linyyy1_t e)
 {
-  ap_yyy0_minimize(e->yyy0);
+  ap_linyyy0_minimize(e->linyyy0);
 }
 
 /* ====================================================================== */
 /* II. Conversions */
 /* ====================================================================== */
 
-bool ap_yyy1_set(ap_yyy1_t res, ap_yyy1_t e, num_internal_t intern)
+bool ap_linyyy1_set(ap_linyyy1_t res, ap_linyyy1_t e, num_internal_t intern)
 {
   if (res!=e){
     ap_environment_set(&res->env,e->env);
-    return ap_yyy0_set(res->yyy0,e->yyy0,intern);
+    return ap_linyyy0_set(res->linyyy0,e->linyyy0,intern);
   }
   else
     return true;
 }
-bool ap_yyy1_set_yyy0(ap_yyy1_t res, ap_yyy0_t e, ap_environment_t* env, num_internal_t intern)
+bool ap_linyyy1_set_linyyy0(ap_linyyy1_t res, ap_linyyy0_t e, ap_environment_t* env, num_internal_t intern)
 {
   ap_environment_set(&res->env,env);
-  return ap_yyy0_set(res->yyy0,e,intern);
+  return ap_linyyy0_set(res->linyyy0,e,intern);
 }
 MACRO_MAINZ
-bool ap_yyy1_set_yyyZZZ(ap_yyy1_t res, ap_yyyZZZ_t e, ap_environment_t* env, num_internal_t intern)
+bool ap_linyyy1_set_linyyyZZZ(ap_linyyy1_t res, ap_linyyyZZZ_t e, ap_environment_t* env, num_internal_t intern)
 {
   ap_environment_set(&res->env,env);
-  return ap_yyy0_set_yyyZZZ(res->yyy0,e,intern);
+  return ap_linyyy0_set_linyyyZZZ(res->linyyy0,e,intern);
 }
-bool ap_yyyZZZ_set_yyy1(ap_yyyZZZ_t res, ap_yyy1_t e, num_internal_t intern)
+bool ap_linyyyZZZ_set_linyyy1(ap_linyyyZZZ_t res, ap_linyyy1_t e, num_internal_t intern)
 {
-  return ap_yyyZZZ_set_yyy0(res,e->yyy0,intern);
+  return ap_linyyyZZZ_set_linyyy0(res,e->linyyy0,intern);
 }
 ENDMACRO
 
@@ -141,38 +141,41 @@ bool ap_linexpr1_set_list(num_internal_t intern, ap_linexpr1_t expr, bool* perro
   return res;
 }
 
-#elif defined(_AP_lincons1_MARK_)
+#elif defined(_AP_lincons1_MARK_) || defined (_AP_lingen1_MARK_)
 
-void ap_lincons1_linexpr1ref(ap_linexpr1_t e, ap_lincons1_t c)
+void ap_linyyy1_linexpr1ref(ap_linexpr1_t e, ap_linyyy1_t c)
 {
-  ap_lincons0_linexpr0ref(e->linexpr0,c->lincons0);
+  ap_linyyy0_linexpr0ref(e->linexpr0,c->linyyy0);
   e->env = c->env;
 }
-ap_constyp_t* ap_lincons1_constypref(ap_lincons1_t c)
-{ return ap_lincons0_constypref(c->lincons0); }
-mpq_ptr ap_lincons1_mpqref(ap_lincons1_t c)
-{ return ap_lincons0_mpqref(c->lincons0); }
-bool ap_lincons1_get_linexpr1(ap_linexpr1_t e, ap_lincons1_t c, num_internal_t intern)
+ap_yyytyp_t* ap_linyyy1_yyytypref(ap_linyyy1_t c)
+{ return ap_linyyy0_yyytypref(c->linyyy0); }
+bool ap_linyyy1_get_linexpr1(ap_linexpr1_t e, ap_linyyy1_t c, num_internal_t intern)
 {
   ap_linexpr1_t ref;
-  ap_lincons1_linexpr1ref(ref,c);
+  ap_linyyy1_linexpr1ref(ref,c);
   return ap_linexpr1_set(e,ref,intern);
 }
-ap_constyp_t ap_lincons1_get_constyp(ap_lincons1_t c)
-{ return ap_lincons0_get_constyp(c->lincons0); }
-void ap_lincons1_get_mpq(mpq_t mpq, ap_lincons1_t c)
-{ ap_lincons0_get_mpq(mpq,c->lincons0); }
+ap_yyytyp_t ap_linyyy1_get_yyytyp(ap_linyyy1_t c)
+{ return ap_linyyy0_get_yyytyp(c->linyyy0); }
 
-bool ap_lincons1_set_linexpr1(ap_lincons1_t c, ap_linexpr1_t e, num_internal_t intern)
+bool ap_linyyy1_set_linexpr1(ap_linyyy1_t c, ap_linexpr1_t e, num_internal_t intern)
 {
   ap_linexpr1_t ref;
-  ap_lincons1_linexpr1ref(ref,c);
+  ap_linyyy1_linexpr1ref(ref,c);
   return ap_linexpr1_set(ref,e,intern);
 }
-void ap_lincons1_set_constyp(ap_lincons1_t c, ap_constyp_t constyp)
-{ ap_lincons0_set_constyp(c->lincons0,constyp); }
+void ap_linyyy1_set_yyytyp(ap_linyyy1_t c, ap_yyytyp_t yyytyp)
+{ ap_linyyy0_set_yyytyp(c->linyyy0,yyytyp); }
+
+#if defined (_AP_lincons0_MARK_)
+mpq_ptr ap_lincons1_mpqref(ap_lincons1_t c)
+{ return ap_lincons0_mpqref(c->lincons0); }
+void ap_lincons1_get_mpq(mpq_t mpq, ap_lincons1_t c)
+{ ap_lincons0_get_mpq(mpq,c->lincons0); }
 void ap_lincons1_set_mpq(ap_lincons1_t c, mpq_t mpq)
 { ap_lincons0_set_mpq(c->lincons0,mpq); }
+#endif
 
 #else
 #error "HERE"
@@ -182,19 +185,19 @@ void ap_lincons1_set_mpq(ap_lincons1_t c, mpq_t mpq)
 /* IV. Change of dimensions and permutations */
 /* ====================================================================== */
 
-void ap_yyy1_extend_environment(ap_yyy1_t nexpr,
+void ap_linyyy1_extend_environment(ap_linyyy1_t nexpr,
 				bool* perror,
-				ap_yyy1_t expr,
+				ap_linyyy1_t expr,
 				ap_environment_t* nenv)
 {
-  if (nexpr->yyy0->discr != expr->yyy0->discr)
+  if (nexpr->linyyy0->discr != expr->linyyy0->discr)
     abort();
-  SWITCH(nexpr->yyy0->discr)
-    ap_yyyXXX_extend_environment(nexpr->yyy0->yyy.XXX,perror,
+  SWITCH(nexpr->linyyy0->discr)
+    ap_linyyyXXX_extend_environment(nexpr->linyyy0->linyyy.XXX,perror,
 				 nenv,
-				 expr->yyy0->yyy.XXX,
+				 expr->linyyy0->linyyy.XXX,
 				 expr->env);
   ENDSWITCH
   ap_environment_set(&nexpr->env,nenv);
 }
-#undef _AP_yyy1_MARK_
+#undef _AP_linyyy1_MARK_
