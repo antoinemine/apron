56a4db4dbeeae59e81fa0c923932cdc15896f6e0
bjeannet <bjeannet@d6d8869d-df28-4c9d-b3d8-ddc8926f8a8a>
2010-09-09 10:36:41 +0000

    Generators fully added

--- apron/branches/newcoeffslinexpr/apron/ap_linexprconsgenXXX_conv.c
+++ apron/branches/newcoeffslinexpr/apron/ap_linexprconsgenXXX_conv.c
@@ -1,8 +1,8 @@
 /* ********************************************************************** */
-/* ap_linexprconsXXX_conv.c: conversion between linear expressions and constraints */
+/* ap_linexprconsgenXXX_conv.c: conversion between linear expressions and constraints */
 /* ********************************************************************** */
 
-#include "ap_linexprconsXXX_conv.h"
+#include "ap_linexprconsgenXXX_conv.h"
 
 #include "boundXXX.h"
 #include "itvXXX.h"
@@ -56,6 +56,23 @@ bool ap_linconsXXX_array_set_linconsZZZ_array(ap_linconsXXX_array_t a, ap_lincon
   }
   return res;
 }
+bool ap_lingenXXX_set_lingenZZZ(ap_lingenXXX_t a, ap_lingenZZZ_t b, num_internal_t intern)
+{
+  bool res;
+  res = ap_linexprXXX_set_linexprZZZ(a->linexpr,b->linexpr,intern);
+  a->gentyp = b->gentyp;
+  return res;
+}
+bool ap_lingenXXX_array_set_lingenZZZ_array(ap_lingenXXX_array_t a, ap_lingenZZZ_array_t b, num_internal_t intern)
+{
+  size_t i;
+  bool res = true;
+  ap_lingenXXX_array_resize(a,b->size);
+  for (i=0; i<b->size; i++){
+    res = ap_lingenXXX_set_lingenZZZ(a->p[i],b->p[i],intern) && res;
+  }
+  return res;
+}
 
 #if !defined(_APZZZ_MARK_) && !defined(_APD_MARK_) && !defined(_APMPQ_MARK_) && !defined(_APMPFR_MARK_)
 bool ap_linexprZZZ_set_linexprXXX(ap_linexprZZZ_t a, ap_linexprXXX_t b, num_internal_t intern)
@@ -84,7 +101,6 @@ bool ap_linconsZZZ_set_linconsXXX(ap_linconsZZZ_t a, ap_linconsXXX_t b, num_inte
   bool res;
   res = ap_linexprZZZ_set_linexprXXX(a->linexpr,b->linexpr,intern);
   a->constyp = b->constyp;
-  mpq_set(a->mpq,b->mpq);
   return res;
 }
 bool ap_linconsZZZ_array_set_linconsXXX_array(ap_linconsZZZ_array_t a, ap_linconsXXX_array_t b, num_internal_t intern)
@@ -94,6 +110,23 @@ bool ap_linconsZZZ_array_set_linconsXXX_array(ap_linconsZZZ_array_t a, ap_lincon
   ap_linconsZZZ_array_resize(a,b->size);
   for (i=0; i<b->size; i++){
     res = ap_linconsZZZ_set_linconsXXX(a->p[i],b->p[i],intern) && res;
+  }
+  return res;
+}
+bool ap_lingenZZZ_set_lingenXXX(ap_lingenZZZ_t a, ap_lingenXXX_t b, num_internal_t intern)
+{
+  bool res;
+  res = ap_linexprZZZ_set_linexprXXX(a->linexpr,b->linexpr,intern);
+  a->gentyp = b->gentyp;
+  return res;
+}
+bool ap_lingenZZZ_array_set_lingenXXX_array(ap_lingenZZZ_array_t a, ap_lingenXXX_array_t b, num_internal_t intern)
+{
+  size_t i;
+  bool res = true;
+  ap_lingenZZZ_array_resize(a,b->size);
+  for (i=0; i<b->size; i++){
+    res = ap_lingenZZZ_set_lingenXXX(a->p[i],b->p[i],intern) && res;
   }
   return res;
 }
