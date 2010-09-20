c4cf8db6191aef881f5fc1c5592c1f38a906be42
Bertrand Jeannet <bertrand.jeannet@argosim.com>
2010-09-20 16:16:26 +0000

    Awful bugs in itv operations corrected (negations in wrong order for correct rounding)

--- itv/itv.h
+++ itv/itv.h
@@ -2,7 +2,7 @@
 /* itv.h: (unidimensional) intervals */
 /* ********************************************************************** */
 
-#ifndef _ITV_H_
+#ifndef _ITVH_
 #define _ITV_H_
 
 #include <stdio.h>
@@ -354,10 +354,10 @@ static inline void itv_set_num(itv_t a, num_t b)
 }
 static inline void itv_set_num2(itv_t a, num_t b, num_t c)
 {
-  bound_set_num(a->sup,b);
-  num_neg(c,c);
-  bound_set_num(a->inf,c);
-  num_neg(c,c);
+  num_neg(b,b);
+  bound_set_num(a->inf,b);
+  num_neg(b,b);
+  bound_set_num(a->sup,c);
 }
 static inline void itv_set_unit_num(itv_t a, num_t b)
 {
