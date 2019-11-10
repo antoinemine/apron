86ac19d8a215aac9a8a553f0825175d99746b0e1
Antoine Miné <antoine.mine@lip6.fr>
2019-11-10 10:51:01 +0100

    preparation for vresion 0.9.12 release

--- version.mk
+++ version.mk
@@ -1,7 +1,7 @@
 # bump here at each release
 VERSION_MAJOR = 0
 VERSION_MINOR = 9
-VERSION_MICRO = 11
+VERSION_MICRO = 12
 
 # automatically generated
 VERSION_STR = "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}"
