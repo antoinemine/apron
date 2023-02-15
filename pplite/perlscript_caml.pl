7d74a58e53b7207d2052cc96b5729c316f6023d0
Enea Zaffanella <enea.zaffanella@unipr.it>
2023-02-15 10:23:05 +0100

    Added ad hoc functions to Caml interface.

--- pplite/perlscript_caml.pl
+++ pplite/perlscript_caml.pl
@@ -2,7 +2,9 @@ while (<>) {
     s/Manager.ap_manager_ptr/'a t Apron.Manager.t/g;
     s/Abstract0.ap_abstract0_ptr/'a t Apron.Abstract0.t/g;
     s/ap_lincons0_t/t/g;
+    s/Lincons0.t/Apron.Lincons0.t/g;
     s/ap_lincons0_array_t/t array/g;
+    s/Lincons0.t array/Apron.Lincons0.t array/g;
     s/ap_tcons0_t/t/g;
     s/ap_tcons0_array_t/t array/g;
     s/external ap_pplite_/external /g;
