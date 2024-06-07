94f6386766872ad59bba93aa3d864d1807c4e050
Antoine Miné <antoine.mine@lip6.fr>
2024-06-07 17:28:47 +0200

    preparation for 0.9.15 release

--- version.mk
+++ version.mk
@@ -1,7 +1,7 @@
 # bump here at each release
 VERSION_MAJOR = 0
 VERSION_MINOR = 9
-VERSION_MICRO = 14
+VERSION_MICRO = 15
 
 # automatically generated
 VERSION_STR = "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}"
